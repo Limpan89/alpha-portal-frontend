290803fcb45ae5acf39cf0e218c42d7fb3d2f1cd
Limpan89 <lbroback89@gmail.com>
2025-04-01 23:06:52 +0200

    Added root

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
+
+export default function App() {
   return (
     <>
       <div>Hello World!</div>
     </>
   );
 }
-
-export default App;
