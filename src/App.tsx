1e8290965f649bf2e145d49bb7349da90fa79bbc
Limpan89 <lbroback89@gmail.com>
2025-04-01 23:08:56 +0200

    Added stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./assets/css/App.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
