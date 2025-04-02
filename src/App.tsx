5a7d43a251fccb1b302b051b8a567a68d82d5aaf
Limpan89 <lbroback89@gmail.com>
2025-04-02 11:00:02 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import "./assets/css/App.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import { routesConfig } from "./routing/RoutesConfig";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
 
 export default function App() {
-  return (
-    <>
-      <div>Hello World!</div>
-    </>
-  );
+  const routing = useRoutes(routesConfig);
+
+  return <>{routing}</>;
 }
