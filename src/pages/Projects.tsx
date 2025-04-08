4c4df7c476fe318e9042eb6576fdd4be9c09c020
Limpan89 <lbroback89@gmail.com>
2025-04-08 23:17:05 +0200

    cards

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,47 @@
+import { ModalButton } from "../components/ModalButton";
+import { ProjectCard } from "../components/ProjectCard";
+
 export const Projects = () => {
   return (
-    <>
-      <div>Projects!</div>
-    </>
+    <div id="projects">
+      <header className="page-header">
+        <h1 className="h1">Projects</h1>
+        <ModalButton type="add" target="#" text="Add Project" />
+      </header>
+      <div className="card-container">
+        <ProjectCard
+          project={"Test Project"}
+          client={"Test Client, Inc."}
+          description={
+            "A test project for testing the design of project cards."
+          }
+          days={5}
+        />
+        <ProjectCard
+          project={"Test Project"}
+          client={"Test Client, Inc."}
+          description={
+            "A test project for testing the design of project cards."
+          }
+          days={5}
+        />
+        <ProjectCard
+          project={"Test Project"}
+          client={"Test Client, Inc."}
+          description={
+            "A test project for testing the design of project cards."
+          }
+          days={5}
+        />
+        <ProjectCard
+          project={"Test Project"}
+          client={"Test Client, Inc."}
+          description={
+            "A test project for testing the design of project cards."
+          }
+          days={5}
+        />
+      </div>
+    </div>
   );
 };
